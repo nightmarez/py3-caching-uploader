36031687052828cecf3c4925b9c5381e89d3dcf2
Mikhail Makarov <m.m.makarov@gmail.com>
2019-05-25 18:18:52 +0300

    update

--- uploader.py
+++ uploader.py
@@ -1,12 +1,12 @@
 # uploader.py
 # Copyright (C) 2018 Mikhail Makarov <m.m.makarov@gmail.com>
-# Welcome to my site https://russiancoders.tech
+# Welcome to my site https://russiancoders.dev/about/
 
 """
 Utility for uploading files and directories over ssh using the scp1 protocol.
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 import os
 from pathlib import Path
@@ -16,6 +16,7 @@ from scp import SCPClient
 import hashlib
 import json
 
+
 def md5(name: str) -> str:
 	hash_md5 = hashlib.md5()
 
@@ -24,6 +25,7 @@ def md5(name: str) -> str:
 			hash_md5.update(chunk)
 
 	return hash_md5.hexdigest()
+
 
 def check_file_changes(name: str) -> bool:
 	my_file = Path(CACHE_FILES)
@@ -42,11 +44,15 @@ def check_file_changes(name: str) -> bool:
 			f.close()
 			break
 
+		line = line.strip()
+
+		if not len(line):
+			continue
+
 		lines.append(line)
 
 	need_load = False
 	found = False
-	curr_hash = False
 
 	f = open(CACHE_FILES, "w")
 	for line in lines:
@@ -59,20 +65,20 @@ def check_file_changes(name: str) -> bool:
 			curr_hash = md5(file_name)
 
 			if curr_hash != file_hash:
-				line = file_name + '|' + curr_hash + '\r\n'
+				line = file_name + '|' + curr_hash
 				need_load = True
 
-		f.write(line)
+		f.write(line + '\n')
 
 	if not found:
 		need_load = True
 		curr_hash = md5(name)
-		line = name + '|' + curr_hash + '\r\n'
-		f.write(line)
+		f.write(name + '|' + curr_hash + '\n')
 
 	f.close()
 
 	return need_load
+
 
 def need_create_dir(name: str) -> bool:
 	my_file = Path(CACHE_DIRS)
@@ -91,6 +97,11 @@ def need_create_dir(name: str) -> bool:
 			f.close()
 			break
 
+		line = line.strip()
+
+		if not len(line):
+			continue
+
 		lines.append(line)
 
 	found = False
@@ -99,12 +110,13 @@ def need_create_dir(name: str) -> bool:
 		if line.strip() == name:
 			found = True
 
-		f.write(line.strip() + '\r\n')
+		f.write(line.strip() + '\n')
 
 	if not found:
-		f.write(name + '\r\n')
+		f.write(name + '\n')
 
 	return not found
+
 
 def need_upload_file(name: str) -> bool:
 	ext = name.split('.')
@@ -115,12 +127,14 @@ def need_upload_file(name: str) -> bool:
 
 	return False
 
+
 def upload_file(name: str, path: str, scp: SCPClient):
 	if check_file_changes(name):
 		print("File '" + name + "' uploading...")
 		scp.put(name, path)
 	else:
 		print("File '" + name + "' skip")
+
 
 def upload_directory(name: str, ssh: SSHClient, scp: SCPClient, recursive: bool = True):
 	print("Observe '" + name + "' directory...")
@@ -138,6 +152,7 @@ def upload_directory(name: str, ssh: SSHClient, scp: SCPClient, recursive: bool 
 		for d in dirs:
 			if d not in ['.', '..', '__pycache__'] and d not in DISALLOWED_DIRECTORIES:
 				upload_directory(name + '/' + d, ssh, scp)
+
 
 def load_config_rules(data, ssh: SSHClient, scp: SCPClient):
 	global ALLOWED_EXTENSIONS
@@ -160,12 +175,14 @@ def load_config_rules(data, ssh: SSHClient, scp: SCPClient):
 		destination_path = file_rule[1] if len(file_rule) > 1 else '.'
 		upload_file(file_name, destination_path, scp)
 
+
 def load_config_cache(data):
 	global CACHE_FILES
 	global CACHE_DIRS
 
-	CACHE_FILES = data['directories']
-	CACHE_DIRS = data['files']
+	CACHE_DIRS = data['directories']
+	CACHE_FILES = data['files']
+
 
 def load_config_server(data):
 	global SERVER_IP
@@ -177,6 +194,7 @@ def load_config_server(data):
 	SERVER_USERNAME = data['user']
 	SERVER_PASSWORD = data['pass']
 	SERVER_DIRECTORY = data['directory']
+
 
 def load_config(before, after):
 	with open('uploader-config.json', 'r') as file:
@@ -194,19 +212,23 @@ def load_config(before, after):
 		finally:
 			after(ssh, scp)
 
+
 def init_ssh():
 	ssh = SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh.connect(SERVER_IP, 22, SERVER_USERNAME, SERVER_PASSWORD)
 	scp = SCPClient(ssh.get_transport())
-	return (ssh, scp)
+	return ssh, scp
+
 
 def close_ssh(ssh: SSHClient, scp: SCPClient):
 	scp.close()
 	ssh.close()
 
+
 def upload():
 	load_config(init_ssh, close_ssh)
+
 
 if __name__ == "__main__":
 	upload()
